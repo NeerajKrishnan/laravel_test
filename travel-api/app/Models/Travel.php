e685e66a689d706eda15b704792d38ebcd915d37
Your Name <you@example.com>
2023-08-28 20:13:50 +0530

    [add] basic functionality

---
+++ travel-api/app/Models/Travel.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Travel extends Model
+{
+    use HasFactory;
+
+    protected $table='travel';
+
+    protected $fillable = ['is_public','slug','name','description','number_of_days'];
+
+}
